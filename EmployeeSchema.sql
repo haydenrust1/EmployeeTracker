f9ff8668f9a94e72da498c5d772d30b03e92cf20
Haydenrust1 <haydenrust1@gmail.com>
2020-10-05 21:45:27 -0700

    working schema

--- EmployeeSchema.sql
+++ EmployeeSchema.sql
@@ -6,47 +6,52 @@ USE employees_DB;
 
 CREATE TABLE department (
     id INT UNSIGNED AUTO_INCREMENT,
-    name VARCHAR(30) UNIQUE NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL, 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE job (
-    id INT UNSIGNED AUTO_INCREMENT,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL(10,2) UNSIGNED NOT NULL,
     department_id INT UNSIGNED NOT NULL,
-    PRIMARY KEY (id)
+    INDEX dep_index (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT UNSIGNED AUTO_INCREMENT,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     job_id INT UNSIGNED NOT NULL,
+    INDEX job_index (job_id),
+    CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES job(id) ON DELETE CASCADE,    
     manager_id INT UNSIGNED,
-    PRIMARY KEY (id)
+    INDEX manager_index (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
 
 INSERT INTO department (name) 
 VALUES ("Sales"),("Engineering"),("Finance"),("Legal");
 
 INSERT INTO job (title, salary, department_id)
-VALUES ("Sales Lead", 100000, (SELECT id FROM department where name = "Sales")),
+VALUES ("Sales Lead", 180000, (SELECT id FROM department where name = "Sales")),
 ("Salesperson", 80000, (SELECT id FROM department where name = "Sales")), 
-("Lead Engineer", 150000, (SELECT id FROM department where name = "Engineering")),
+("Lead Engineer", 190000, (SELECT id FROM department where name = "Engineering")),
 ("Software Engineer", 120000, (SELECT id FROM department where name = "Engineering")),
 ("Accountant", 125000, (SELECT id FROM department where name = "Finance")),
 ("Human Relations", 100000,(SELECT id FROM department where name = "Legal")),
-("Lawyer", 100000, (SELECT id FROM department where name = "Finance"));
+("Lawyer", 200000, (SELECT id FROM department where name = "Finance"));
 
 INSERT INTO employee (first_name, last_name, job_id)
 VALUES ("John", "Doe", (SELECT id FROM job where title = "Sales Lead")), 
-("Mike", "Chan",(SELECT id FROM job where title = "Salesperson")), 
-("Ashley", "Rodriguez",(SELECT id FROM job where title = "Lead Engineer")), 
-("Kevin", "Tupik",(SELECT id FROM job where title = "Software Engineer")),
-("Malia", "Brown",(SELECT id FROM job where title = "Accountant")), 
-("Sarah", "Lourd",(SELECT id FROM job where title = "Legal Team Lead")), 
-("Tom", "Allen",(SELECT id FROM job where title = "Lawyer"));
+("Madison", "Rust",(SELECT id FROM job where title = "Salesperson")), 
+("Hayden", "Rust",(SELECT id FROM job where title = "Lead Engineer")), 
+("Kevin", "Soule",(SELECT id FROM job where title = "Software Engineer")),
+("Delaney", "O'Haver",(SELECT id FROM job where title = "Software Engineer")),
+("Emiley", "Grose",(SELECT id FROM job where title = "Accountant")), 
+("Tim", "Salz",(SELECT id FROM job where title = "Human Relations")), 
+("Josh", "Hamilton",(SELECT id FROM job where title = "Lawyer"));
 
 UPDATE employee
 SET manager_id = 3
